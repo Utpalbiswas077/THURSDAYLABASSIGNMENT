0291905dc17452b50903382db85d4aa061f7a9d0
Utpalbiswas077 <106583890+Utpalbiswas077@users.noreply.github.com>
2022-07-04 19:12:25 +0530

    Add files via upload

--- ShortArryElements.java
+++ ShortArryElements.java
@@ -1,21 +1,35 @@
 package Opp.Arry;
 
+import java.util.Scanner;
+
 public class ShortArryElements {
 	 public static void main(String[] args) {        
          
 	        //Initialize array     
-	        int a[]={5, 2, 8, 7, 1};     
-	        int temp = 0;    
+	       // int a[]={5, 2, 8, 7, 1}; 
+		 
+		 Scanner sc = new Scanner(System.in);// creat scanner class 
+		 
+			System.out.print(" Please Enter Number of elements in an array : ");
+			int Size = sc.nextInt();
+	
+			int  a[] = new int[Size]; 
+	        int temp = 0; 
+	        System.out.print(" Please Enter " + Size + " elements of an Array  : ");
+			for (int i = 0; i < Size; i++) // loop for arry a 
+			{
+				a[i] = sc.nextInt(); // getting in put for elements of arry a
+			}   
 	            
 	        //Displaying elements of original array    
 	        System.out.println("Elements of original array: ");    
-	        for (int i = 0; i < a.length; i++) {     
+	        for (int i = 0; i < Size; i++) {     
 	            System.out.print(a[i] + " ");    
 	        }    
 	            
 	        //Sort the array in ascending order    
-	        for (int i = 0; i < a.length; i++) {     
-	            for (int j = i+1; j < a.length; j++) {     
+	        for (int i = 0; i < Size; i++) {     
+	            for (int j = i+1; j < Size; j++) {     
 	               if(a[i] > a[j]) {    
 	                   temp = a[i];    
 	                   a[i] = a[j];    
@@ -28,7 +42,7 @@ public class ShortArryElements {
 	            
 	        //Displaying elements of array after sorting    
 	        System.out.println("Elements of array sorted in ascending order: ");    
-	        for (int i = 0; i < a.length; i++) {     
+	        for (int i = 0; i <Size; i++) {     
 	            System.out.print(a[i] + " ");    
 	        }    
 	    }    
