fb6ed6957e6808b4a7a15a4c5c4f6195edc088d5
Utpalbiswas077 <106583890+Utpalbiswas077@users.noreply.github.com>
2022-07-04 19:18:22 +0530

    Add files via upload

--- ReverseArry.java
+++ ReverseArry.java
@@ -1,22 +1,31 @@
 package Opp.Arry;
 
+import java.util.Scanner;
+
 public class ReverseArry {
 	
 	public static void main(String[] args) 
 	{  
-        //Initialize array  
-		
-        int a[] = new int [] {1, 2, 3, 4, 5};  
-        System.out.println("Original array: ");  
-        for (int i = 0; i < 5; i++) {  
-            System.out.println(a[i] + " ");  
-        }  
-            
+        //Initialize array 
+		 Scanner sc = new Scanner(System.in);// creat scanner class 
+		 
+			System.out.print(" Please Enter Number of elements in an array : ");
+			int Size = sc.nextInt();
+	
+			int  a[] = new int[Size]; 
+	        int temp = 0; 
+	        System.out.print(" Please Enter " + Size + " elements of an Array  : ");
+			for (int i = 0; i < Size; i++) // loop for arry a 
+			{
+				a[i] = sc.nextInt(); // getting in put for elements of arry a
+			}   
+	            
+	        System.out.println();
         System.out.println("Array in reverse order: ");  
         //Loop through the array in reverse order 
       for(int i=5-1;i>=0;i--)
       {
-    	   System.out.println(a[i] + " "); 
+    	   System.out.print(a[i] + " "); 
         }  
     }  
 }  
