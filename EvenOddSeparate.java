9d2c021afa64e2e8ffd0411ab6336edaf71e5daa
Utpalbiswas077 <106583890+Utpalbiswas077@users.noreply.github.com>
2022-07-04 19:06:00 +0530

    Add files via upload

--- EvenOddSeparate.java
+++ EvenOddSeparate.java
@@ -6,24 +6,30 @@ public class EvenOddSeparate {
 
 	public static void main(String args[]) {
 
-		int a[]={1,2,5,6,3,8,12};  
-		
+		int a[]={1,2,5,6,3,8,12}; 
+		 System.out.println("Elements of original array: ");    
+	        for (int i = 0; i < a.length; i++) {     
+	            System.out.print(a[i] + " ");
+	            }
+	        
+	        System.out.println();
 		System.out.println("Odd Numbers:");  
 		// loop for check odd number
 		for(int i=0;i<a.length;i++)
 		{  
 		if(a[i]%2!=0) //check odd number
 		{  
-		System.out.println(a[i]);  
+		System.out.print(a[i]+" ");  
 		}  
-		}  
+		} 
+		System.out.println();
 		System.out.println("Even Numbers:");
 		// Loop for check even number
 		for(int i=0;i<a.length;i++)
 		{  
 		if(a[i]%2==0) // check even
 		{  
-		System.out.println(a[i]);  
+		System.out.print(a[i]+" ");  
 		}  
 		}  
 		}
